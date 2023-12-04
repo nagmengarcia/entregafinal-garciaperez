fe73f1d2f7c364a7827c6625be153944fa771be0
nagmengarcia <nagmengarcia@gmail.com>
2023-12-04 01:20:23 -0300

    funciona el menu mobile desplegable

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
   const menu = document.getElementById("mobile-and-tablet-menu-button");
   const menuItems = document.getElementById("menu-items");
-  const menuText = document.getElementById("menu-text");
+  const textoParaCambiar = document.getElementById("menu-text");
+
+  let textoCambiado = false;
 
   menu.addEventListener("click", function () {
     menuItems.classList.toggle("appear");
+    /*textoParaCambiar.textContent = "cerrar";*/
+    if (textoCambiado) {
+      textoParaCambiar.textContent = "menú";
+    } else {
+      textoParaCambiar.textContent = "cerrar"
+    }
+    
+    textoCambiado = !textoCambiado;
+  }, 10);
 
 
-  });
 });
 
